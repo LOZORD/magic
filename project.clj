162cda4a5c4c54ea9745e821de890ae3f1b99758
LOZORD <ljrudberg@gmail.com>
2015-07-12 23:47:09 -0700

    Added project.clj file

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject magic "0.0.1-SNAPSHOT"
+  :description "Many of Ahl's Games In Clojure"
+  :url "http://github.com/LOZORD/magic"
+  :license {:name "GPL"
+            :url "https://github.com/LOZORD/magic/blob/master/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :plugins [[lein-checkall "0.1.1"]])
