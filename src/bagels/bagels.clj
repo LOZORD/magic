8ceb215907bd065c576182a557d09cd945897232
LOZORD <ljrudberg@gmail.com>
2015-08-16 20:01:45 -0700

    Game works much better now

--- src/bagels/bagels.clj
+++ src/bagels/bagels.clj
@@ -8,7 +8,7 @@
 (def MAX
   (- (int (Math/pow 10 secret-length)) 1))
 (def upcase upper-case)
-(def pcaps (comp println upcase))
+(def pcaps (comp println upcase str))
 (def num-chars [\0 \1 \2 \3 \4 \5 \6 \7 \8 \9])
 (def zip  (partial map vector))
 (def none (comp not boolean some))
@@ -17,20 +17,33 @@
          build-fermi build-final zero-pad pico-fermi-print bagels?)
 
 (defn print-help []
-  (pcaps "TODO: help text"))
+  (pcaps "I am thinking of a three digit number (including zeros)\n"
+         "your goal is to attempt to guess my number (in ≤ 20 guesses) with the following hints\n"
+         "\tpico\t\tone digit correct, but in the wrong position\n"
+         "\tfermi\t\tone digit correct and in the right position\n"
+         "\tbagels\t\tno digits correct"))
+
+;;(defn new-secret [& seed]
+;;  (let [pre (str (rand-int (+ MAX 1)))
+;;        secret (zero-pad pre)]
+;;    (secret)))
+
+(defn new-secret [& seed]
+  (zero-pad (str (rand-int (inc MAX)))))
+
+(defn new-game [& seed]
+  (play-round (new-secret) num-guesses))
 
 (defn run-game [& seed] ;; TODO: something with `seed`
-  (do
-    (print-help)
-    (let [pre (str (rand-int (+ MAX 1))) ;; TODO refactor into separate function
-          secret (zero-pad pre)]
-          ;;c (count pre)
-          ;;secret (if (= c secret-length)
-          ;;         pre
-          ;;         (concat (take (- secret-length c) ["0" "0"]) pre))]
-      (play-round secret num-guesses))))
+  (do (print-help) (new-game seed)))
 
-(defn lose-state [] (pcaps "YOU LOSE!"))
+(defn win-state []
+  (do (pcaps "you win!") (new-game)))
+(defn lose-state []
+  (do (pcaps "you lose!") (new-game)))
+
+(defn get-user-guess []
+  (zero-pad (take secret-length (trim (read-line)))))
 
 (defn play-round [secret guesses]
   (if (> guesses 0)
@@ -38,14 +51,13 @@
       (pcaps (str "guess #" (+ (- num-guesses guesses) 1) "?"))
       (flush)
       (let [user-guess (zero-pad (trim (read-line))) ;;(zero-pad (take secret-length (trim (read-line)))) TODO only take a certain amount
+            ;; XXX user-guess (get-user-guess)
             ;;foo (pcaps (str "received: " user-guess "\tsecret " secret))
             correct? (= user-guess secret)
             result (doall (get-result secret user-guess))]
             ;;bar (doall (pcaps (doall result)))]
         (if correct?
-          (do
-            (pcaps "you got it!")
-            (win-state))
+          (win-state)
           (do
             (if (bagels? result)
               (pcaps "bagels")
@@ -60,8 +72,9 @@
   (let [strings (map #(cond
                         (:pico?  %) "pico"
                         (:fermi? %) "fermi"
-                        :else "") result)
-        single-string (join " " strings)]
+                        :else nil) result)
+        sans-nil (remove nil? strings)
+        single-string (join " " sans-nil)]
     ((comp pcaps trim) single-string)))
 
 (defn get-result [secret user]
@@ -90,5 +103,3 @@
       2 (str "0" s))
     s))
 
-(defn win-state []
-  (pcaps "you won...")) ;; TODO
