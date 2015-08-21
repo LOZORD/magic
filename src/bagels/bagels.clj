292e20bea5c3e6f2648472873d5222bdd0937112
LOZORD <ljrudberg@gmail.com>
2015-08-20 21:41:39 -0700

    Only allow for distinct digits, removed some cruft, added usability

--- src/bagels/bagels.clj
+++ src/bagels/bagels.clj
@@ -17,31 +17,39 @@
          build-fermi build-final zero-pad pico-fermi-print bagels?)
 
 (defn print-help []
-  (pcaps "I am thinking of a three digit number (including zeros)\n"
+  (pcaps "I am thinking of a three digit number (including zeros, all digits distinct)\n"
          "your goal is to attempt to guess my number (in ≤ 20 guesses) with the following hints\n"
          "\tpico\t\tone digit correct, but in the wrong position\n"
          "\tfermi\t\tone digit correct and in the right position\n"
          "\tbagels\t\tno digits correct"))
 
-;;(defn new-secret [& seed]
-;;  (let [pre (str (rand-int (+ MAX 1)))
-;;        secret (zero-pad pre)]
-;;    (secret)))
-
 (defn new-secret [& seed]
-  (zero-pad (str (rand-int (inc MAX)))))
+  (let [secret (zero-pad (str (rand-int (inc MAX))))]
+    ;; so this is kind of a hack so that the logic
+    ;; for detecting picos and fermis is much easier
+    ;; i.e. we won't have duplicate digits
+    ;; still, 720/1000 is pretty good!
+    (if (apply distinct? (seq secret))
+      secret
+      (new-secret seed))))
+
 
 (defn new-game [& seed]
   (play-round (new-secret) num-guesses))
 
 (defn run-game [& seed] ;; TODO: something with `seed`
-  (do (print-help) (new-game seed)))
+  (do
+    (print-help)
+    (new-game seed)))
 
 (defn win-state []
-  (do (pcaps "you win!") (new-game)))
+  (do
+    (pcaps "\tyou win!\n")
+    (new-game)))
+
 (defn lose-state [secret]
   (do
-    (pcaps "you lose!")
+    (pcaps "\tyou lose!\n")
     (pcaps "my number was: " secret)
     (new-game)))
 
@@ -54,11 +62,8 @@
       (pcaps (str "guess #" (+ (- num-guesses guesses) 1) "?"))
       (flush)
       (let [user-guess (zero-pad (trim (read-line))) ;;(zero-pad (take secret-length (trim (read-line)))) TODO only take a certain amount
-            ;; XXX user-guess (get-user-guess)
-            ;;foo (pcaps (str "received: " user-guess "\tsecret " secret))
             correct? (= user-guess secret)
             result (doall (get-result secret user-guess))]
-            ;;bar (doall (pcaps (doall result)))]
         (if correct?
           (win-state)
           (do
